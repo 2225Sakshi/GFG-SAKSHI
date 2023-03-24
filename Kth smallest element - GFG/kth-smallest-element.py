86cb9dded7332fd9c37cfd130288ec9c6ad416d8
Algo Sakshi <112638074+2225Sakshi@users.noreply.github.com>
2023-03-24 15:42:40 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.py
+++ Kth smallest element - GFG/kth-smallest-element.py
@@ -1,9 +1,14 @@
 #User function Template for python3
-
+import heapq
 class Solution:
     def kthSmallest(self,arr, l, r, k):
-        arr.sort()
-        return arr[k-1]
+        # arr.sort()
+        # return arr[k-1]
+        
+        heapq.heapify(arr)
+        for i in range(k-1):
+            heapq.heappop(arr)
+        return heapq.heappop(arr)
         
         '''
         arr : given array
