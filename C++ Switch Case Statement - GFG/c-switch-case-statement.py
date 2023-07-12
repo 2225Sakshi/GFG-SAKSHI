5afae89061612f9d0a0eb2c1c523571e2311583b
Algo Sakshi <112638074+2225Sakshi@users.noreply.github.com>
2023-07-12 23:24:47 +0530

    Added solution - LeetHub

---
+++ C++ Switch Case Statement - GFG/c-switch-case-statement.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+class Solution:
+    def isInRange (ob,N):
+        return {
+            1: 'one',
+            2: 'two',
+            3: 'three',
+            4: 'four',
+            5: 'five',
+            6: 'six',
+            7: 'seven',
+            8: 'eight',
+            9: 'nine',
+            10: 'ten'
+        }.get(N, 'not in range')
+        
+        
+        # code here 
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        
+        N=int(input())
+
+        ob = Solution()
+        print(ob.isInRange(N))
+# } Driver Code Ends
